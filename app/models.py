7634eeb0d00d51bd50c4c904135dea02fbe88071
hyderalimaroof <hyderalimaroof@gmail.com>
2022-02-01 11:13:33 +0500

    initial commit

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from datetime import date
+from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ImageField
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+    created_date = models.DateField(default = date.today())
+    active = models.BooleanField(default=True)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    body = models.TextField()
+    created_date = models.DateField(date.today())
+    image = models.ImageField(default="profile_pics/default.xyz", upload_to="profile_pics")
+    catogory = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
